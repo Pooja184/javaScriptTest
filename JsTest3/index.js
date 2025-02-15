24c084d53c6a50e884e7fd2d953f7906f6077d40
pxxja <poojabhambid2004@gmail.com>
2025-02-15 14:05:26 +0530

    Awdiz test 3

---
+++ JsTest3/index.js
@@ -0,0 +1,72 @@
+// find number
+
+let nums=[1,2,3,4,5,6];
+let target=6;
+
+const binaryTarget=(nums,target)=>{
+   let left=0;
+   let right=nums.length-1; //last index
+   
+       
+}
+
+console.log(binaryTarget(nums,target));
+
+console.log("Find the second largest number in an array without sorting the array");
+let arr=[0,1,2,3,4,5,6,7,8,9,10,11];
+ const findLargest=(arr)=>{
+       let first=arr[0];
+       let second=arr[0];
+      //  console.log(first,"first")
+       for(let i=0;i<arr.length;i++){
+         // console.log(arr[0])
+          if(arr[i]>first){ //1>1
+            // console.log(arr[i])
+                second=first;
+               //  console.log(second,"second")
+                first=arr[i];
+          }
+          else if(arr[i]>second && arr[i]!==first){
+            // console.log(second,"second")
+                second=arr[i];
+          }
+       }
+       return second;
+ }
+console.log(findLargest(arr));
+
+
+ console.log("Reverse Words in a Sentence Without Using Built-in Methods. the output will be olleh dlrow");
+
+let sentence="hello world";
+
+const reverseSentence=(sentence)=>{
+   // console.log(sentence.length-1)
+   for(let i=sentence.length-1;i>=0;i--){
+        console.log(sentence[i]);
+   
+   }
+
+}
+
+reverseSentence(sentence);
+
+
+// console.log("string compression");
+//  let str="aappaaa";
+
+//  const strC=(str)=>{
+//    let val=0;
+//      for(let i=0;i<=str.length-1;i++){
+//       // console.log(str[i])
+//       if(str[i]===str[i]){
+//           console.log(str[i],i)
+//       }
+//      }
+//    //   console.log(val)
+//  }
+
+//  strC(str);
+
+console.log("Find the second largest number in an array without sorting the array");
+
